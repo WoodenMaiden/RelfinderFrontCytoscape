f0dc67fd5f627d6113243eb1f52dd2dbc66abe95
WoodenMaiden <yann.pomie@laposte.net>
2022-05-12 09:56:12 +0200

    temp commit to ammend

--- src/components/GraphCanvas.js
+++ src/components/GraphCanvas.js
@@ -8,6 +8,9 @@ import "./GraphCanvas.css"
 
 import CanvasButtons from "./CanvasButtons";
 
+
+var lastInputs
+var lastGraph // we use var so we can access these from the component
 export default function GraphCanvas(props) {
 
 
@@ -21,6 +24,41 @@ export default function GraphCanvas(props) {
  */
 
 	cytoscape.warnings(false)
+
+	function draw (graph) {
+		//constructing graph
+		graph.forEachNode((node, attributes) => {
+			cy.add({
+				group: 'nodes',
+				data: {
+					id: node,
+					label: node
+				},
+				selected: false,
+				selectable: true,
+				locked: false,
+				grabbable: true,
+				classes: (!node.match(/^.+:\/\/.*/ig)) ? ['Literal'] : ['Entity']
+			})
+		})
+
+		graph.forEachDirectedEdge((edge, attributes, source, target,
+								sourceAttributes, targetAttributes, undirected) => {
+			cy.add({
+				group: 'edges',
+				data: {
+					id: edge,
+					source: source,
+					target: target,
+					label: attributes.value
+				},
+				pannable: true
+			})
+		})
+
+	}
+
+
 	const layoutOptions = {
 		name: 'breadthfirst',
 
@@ -30,7 +68,7 @@ export default function GraphCanvas(props) {
 
 	let cy = cytoscape();
 	// we are doing this in a use effect since it is executed after the dom finished rendering
-	useEffect(() => {
+	useEffect(() => {		
 		 cy = cytoscape({
 			//visual
 			container: document.getElementById("cyroot"),
@@ -88,6 +126,21 @@ export default function GraphCanvas(props) {
 			pixelRatio: 'auto'
 		})
 
+
+		if (lastGraph && props.nodes.equals(lastInputs)) {
+//			cy.removeData()
+//			cy.remove(cy.nodes().union(cy.nodes().connectedEdges()));
+
+			draw(lastGraph)
+							
+			console.log("drawn in hook")
+			console.log(cy.elements().length)		
+			cy.layout(layoutOptions).run()
+			zoomRatioBtn = cy.zoom() / 2
+
+		}
+			
+
 		cy.on("mouseover", e => {
 			if (!searchMode && e.target !== cy && e.target.isNode()) {
 				for (const elt of cy.elements()) {
@@ -118,6 +171,32 @@ export default function GraphCanvas(props) {
 
 
 	
+	// https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+	Array.prototype.equals = function (array) {
+		// if the other array is a falsy value, return
+		if (!array)
+			return false;
+	
+		// compare lengths - can save a lot of time 
+		if (this.length !== array.length)
+			return false;
+	
+		for (var i = 0, l=this.length; i < l; i++) {
+			// Check if we have nested arrays
+			if (this[i] instanceof Array && array[i] instanceof Array) {
+				// recurse into the nested arrays
+				if (!this[i].equals(array[i]))
+					return false;       
+			}           
+			else if (this[i] !== array[i]) { 
+				// Warning - two different object instances will never be equal: {x:20} != {x:20}
+				return false;   
+			}           
+		}       
+		return true;
+	}
+	// Hide method from for-in loops
+	Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
 /*
      _   _                 _ _
@@ -196,66 +275,54 @@ export default function GraphCanvas(props) {
 	}
 
 
+
+
 	if (props.nodes.length >= 2) {
-		const graph = new MultiDirectedGraph()
-		graph.clear()
-		const myHeaders = new Headers();
-		myHeaders.append("Content-Type", "application/json");
+			if (!props.nodes.equals(lastInputs)) {
+				const graph = new MultiDirectedGraph()
+				graph.clear()
+				const myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
 
-		const raw = JSON.stringify({
-			"nodes": props.nodes
-		});
+				const raw = JSON.stringify({
+					"nodes": props.nodes
+				});
 
-		const requestOptions = {
-			method: "POST",
-			headers: myHeaders,
-			body: raw,
-			redirect: "follow"
-		};
+				const requestOptions = {
+					method: "POST",
+					headers: myHeaders,
+					body: raw,
+					redirect: "follow"
+				};
 
-		const URL = (process.env.REACT_APP_API_URL.slice(-1) === "/") ? process.env.REACT_APP_API_URL.slice(0, -1) : process.env.REACT_APP_API_URL
 
-		fetch(`${URL}/relfinder/2`, requestOptions).then((res) => res.json().then((data) => {
-			graph.import(data);
+				const URL = (process.env.REACT_APP_API_URL.slice(-1) === "/") ? process.env.REACT_APP_API_URL.slice(0, -1) : process.env.REACT_APP_API_URL
 
-			cy.removeData()
-			cy.remove(cy.nodes().union(cy.nodes().connectedEdges()));
+				fetch(`${URL}/relfinder/2`, requestOptions).then((res) => res.json().then((data) => {
+					graph.import(data);
+					console.log("built")
+					cy.removeData()
+					cy.remove(cy.nodes().union(cy.nodes().connectedEdges()));
 
-			//constructing graph
-			graph.forEachNode((node, attributes) => {
-				cy.add({
-					group: 'nodes',
-					data: {
-						id: node,
-						label: node
-					},
-					selected: false,
-					selectable: true,
-					locked: false,
-					grabbable: true,
-					classes: (!node.match(/^.+:\/\/.*/ig)) ? ['Literal'] : ['Entity']
-				})
-			})
+					draw(graph)
+					console.log(cy.elements().length)
+					cy.layout(layoutOptions).run()
+					zoomRatioBtn = cy.zoom() / 2
+					lastInputs = props.nodes
+					lastGraph = graph
+				}))
+			} else {
 
-			graph.forEachDirectedEdge((edge, attributes, source, target,
-									  sourceAttributes, targetAttributes, undirected) => {
-				cy.add({
-					group: 'edges',
-					data: {
-						id: edge,
-						source: source,
-						target: target,
-						label: attributes.value
-					},
-					pannable: true
-				})
+//				cy.removeData()
+//				cy.remove(cy.nodes().union(cy.nodes().connectedEdges()));
 
-			})
-			cy.ready(() => {
+
+				console.log("drawn")
+				console.log(cy.elements().length)		
 				cy.layout(layoutOptions).run()
 				zoomRatioBtn = cy.zoom() / 2
-			})
-		}))
+
+			}
 	}
 
 
